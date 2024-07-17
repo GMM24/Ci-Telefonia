99f5e254aabdb6de07c0afce39269db615f83a16
Genesis Diaz <genesismazariegos006@gmail.com>
2024-07-17 11:15:03 -0600

    Ci-Telefonia

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/fs2024/ADHD02/ci_telefonia/public/';
+    public string $baseURL = 'http://localhost/fs2024/ci_telefonia/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
